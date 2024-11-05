08fefac70d5cd389bb5ca49b8f89b36cad2c4727
Esraa-re <ereda0458@gmail.com>
2024-11-06 01:46:08 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+let allData = [];
+function getData(type) {
+    let myhttp = new XMLHttpRequest();
+
+    //connection established
+    myhttp.open("GET", `https://forkify-api.herokuapp.com/api/search?q=${type}`);
+
+    //send request to server
+    myhttp.send();
+
+    //recieve response 
+    myhttp.addEventListener("readystatechange", function () {
+        if (myhttp.readyState == 4) {
+            let data = JSON.parse(myhttp.response);
+            allData = data.recipes;
+            console.log(allData);
+            display();
+
+
+        }
+    });
+}
+
+
+//call getData
+getData("pizza");
+
+//display data
+function display() {
+    let recipe = ``;
+    for (let i = 0; i < allData.length; i++) {
+        recipe += ` <div class="col-md-3 col-xs-12" id="recipe">
+           <a href="${allData[i].source_url}" target="_blank" ><img src="${allData[i].image_url}" alt="Recipes-photo" class="w-100 shadow"/></a>
+           <a href="${allData[i].source_url}" target="_blank" > <h3 class="fs-5 text-center pt-2 shadow">${allData[i].title}</h3></a>
+        </div>`
+    }
+    document.querySelector("#myData").innerHTML = recipe;
+}
+
+let select = document.querySelector("select");
+select.addEventListener("change", function (e) {
+    getData(e.target.value);
+});
